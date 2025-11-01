3d3a9f4223078987479a42fe401d3af85be4f826
vyleene <lukeruaya700@gmail.com>
2025-11-01 22:33:33 +0800

    Changed text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,16 +11,17 @@ const Header: React.FC = () => {
                 <Row className="gx-5 align-items-center">
                     <Col lg={6}>
                         <div className="mb-5 mb-lg-0 text-center text-lg-start">
-                        <h1 className="display-1 lh-1 mb-3"><b>AlabID. <br />I love ID. <br /> We love it.</b></h1>
+                        <h1 className="display-1 lh-1 mb-3"><b>Lorem. <br />Ipsum. <br /> Dolor sit amet.</b></h1>
                         <Button variant="primary" size="lg" className="rounded-pill">
                             <span className="d-flex align-items-center">
                             <i className="bi-sliders me-2"></i>
-                            <span className="small">Features</span>
+                            <span className="small">Learn more</span>
                             </span>
                         </Button>
                         </div>
                     </Col>
                     <Col lg={6}>
+                        
                     </Col>
                 </Row>
             </Container>
