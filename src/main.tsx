508ad60abe4b9f531cf19de5993511ba8e0bf573
vyleene <lukeruaya700@gmail.com>
2025-11-01 20:56:04 +0800

    Imported bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
 import App from './App.tsx'
 import './index.css'
 
