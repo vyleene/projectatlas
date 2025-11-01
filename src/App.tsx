c5ffeb64ce676d93757f15126e3d2b70f37b04fd
vyleene <lukeruaya700@gmail.com>
2025-11-01 20:56:15 +0800

    Added navbar and hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
 import './App.css'
+import Header from './components/Header'
+import Navigation from './components/Navigation'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Michael the goon lord was here</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          I gooned {count} times now.
-        </button>
-      </div>
+      <Navigation/>
+      <Header/>
     </>
   )
 }
